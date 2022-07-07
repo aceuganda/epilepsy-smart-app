cf22a0a5d94f4e723938c8bc5b98809d0dfd6da9
FaithDaka <faithbusigu8@gmail.com>
2022-07-07 18:35:06 +0300

    home page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,7 +1,11 @@
 import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import Footer from '../../components/layouts/Footer';
+import Card from './Card';
+import SeizureImg from '../../assets/img/HomePage/seizure.png'
+import MedicineImg from '../../assets/img/HomePage/medication.png'
+import ActivitiesImg from '../../assets/img/HomePage/activities.png'
+import TrackImg from '../../assets/img/HomePage/tracking.png'
 
 const HomePage = () => {
     const check = () => {
@@ -12,44 +16,26 @@ const HomePage = () => {
     })
 
     return (
-        <div className='smart home-page'>
-            <div className="row">
-                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <div className='home-header'>
-                        <div className='top-section container-fluid'>
-                            <div>
-                                <span>Holla!</span>
-                                <span><NotificationsNoneIcon/></span>
-                            </div>
-                        </div>
-                        <div className='banner container-fluid'>
-                            <div className='row justify-content-between'>
-                                <span className='col container-fluid left'>
-                                    <i className='fa fa-user font-size-50' style={{borderRadius:"70%", border:'1px solid #aaa', padding:'8px'}}></i>
-                                    <p>Sophie Mukasa</p>
-                                </span>
-                                <span className='col container-fluid right'>
-                                    <h3>Discover Activities <hr/></h3>
-                                    <p>Give yourself a chance.</p>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='container-fluid'>
-                        <div className='home-cards'>
-                            <div className='col justify-content-between'>
-                                <div className='row mb-5'>
-                                    <div className='card col-5 mr-5 text-uppercase font-size-10 font-weight-600'><Link to="/seizure-form"> Seizure Tracking</Link></div>
-                                    <div className='card col-5 text-uppercase font-size-10 font-weight-600'> Medication</div>
-                                </div>
-                                <div className='row mt-5'>
-                                    <div className='card col-5 mr-5 text-uppercase font-size-10 font-weight-600'> Resilience tracking</div>
-                                    <div className='card col-5 text-uppercase font-size-10 font-weight-600'> Resilience Activities</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        <div className='home-page'>
+            <header className='row justify-content-between'>
+                <h4>Holla!</h4>
+                <div><NotificationsNoneIcon/></div>
+            </header>
+            <div className='banner'>
+                <span>
+                    <img/>
+                    <span className='name'>Sophie Mukasa</span>
+                </span>
+                <div>
+                    <h3>Discover Activities <hr/></h3>
+                    <span>Give yourself a chance</span>
                 </div>
+            </div>
+            <div className='cards'>
+                <Card title='seizure tracking' img={SeizureImg} link='/seizure-form'/>
+                <Card title='medication' img={MedicineImg}/>
+                <Card title='resilience tracking' img={TrackImg} />
+                <Card title='resilience activities' img={ActivitiesImg}/>
             </div>
             <Footer />
         </div>
