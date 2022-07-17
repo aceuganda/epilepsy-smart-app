c0f17f335e3573d7231a61c9ea82ddfc2e2e20f3
FaithDaka <faithbusigu8@gmail.com>
2022-07-17 12:17:14 +0300

    Seizure flow

--- src/pages/SeizureMonitoring/SeizureAssessment.js
+++ src/pages/SeizureMonitoring/SeizureAssessment.js
@@ -1,37 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import Form from '../../components/form/Form'
 import TopBar from '../../components/form/TopBar'
-import Question from './components/Question'
+import Question from '../../components/form/Question'
 import { connect, useDispatch } from 'react-redux'
 import { addResponse } from '../../data/redux/actions'
+import PopQuestion from '../../components/form/PopQuestion'
+import SeizureComponent from '.'
 
-let Seizure = () => {
+let SeizureAssessment = () => {
   let dispatch = useDispatch();
-  const [feel, setFeel] = useState();
+  const [aura, setAura] = useState();
   const [engage, setEngage] = useState();
   const [seizure, setSeizure] = useState();
 
   useEffect(() => {
-    console.log('Seizure form values', feel, engage, seizure)
+    console.log('Seizure form values', aura, engage, seizure)
   })
 
   return (
-    <div>
-      <TopBar title='Seizure Tracking' route="/home" />
+    <SeizureComponent backroute={'/seizure-form'}>
       <Form>
-        <form style={{ paddingTop: '20px' }} onSubmit={(e) => {
+        <form onSubmit={(e) => {
           e.preventDefault()
-          dispatch(addResponse(feel, engage, seizure))
+          dispatch(addResponse(aura, engage, seizure))
         }}>
-          <Question question='How do you feel today?' />
-          <fieldset className='mt-3 mb-4'>
-            <button type="button" className='button form-button-pill' value={'Great'} onClick={(e) => { setFeel(e.target.value)}}>Great</button>
-            <button type="button" className='button form-button-pill' value={'Good'} onClick={(e) => { setFeel(e.target.value) }}>Good</button>
-            <button type="button" className='button form-button-pill' value={'Okay'} onClick={(e) => { setFeel(e.target.value) }}>Okay</button>
-            <button type="button" className='button form-button-pill' value={'Bad'} onClick={(e) => { setFeel(e.target.value) }}>Bad</button>
-            <button type="button" className='button form-button-pill' value={'Really Bad'} onClick={(e) => { setFeel(e.target.value) }}>Really Bad</button>
-          </fieldset>
-          <Question question='How did you engage socially?' />
+          <PopQuestion question={'Before the seizure, did you have an '} popTitle={'Aura'}>
+            <fieldset className='mt-3 mb-4'>
+              <button type="button" className='button form-button-pill text-uppercase' value={'Yes'} onClick={(e) => { setAura(e.target.value) }}>yes</button>
+              <button type="button" className='button form-button-pill text-uppercase' value={'No'} onClick={(e) => { setAura(e.target.value) }}>no</button>
+            </fieldset>
+          </PopQuestion>
+          {/* <Question question='How did you engage socially?' />
           <fieldset className='mt-3 mb-4'>
             <button type="button" className='button form-button-pill' value={'School'} onClick={(e) => { setEngage(e.target.value) }}>School</button>
             <button type="button" className='button form-button-pill' value={'Work'} onClick={(e) => { setEngage(e.target.value) }}>Work</button>
@@ -43,13 +42,13 @@ let Seizure = () => {
           <fieldset className='mt-3 mb-4'>
             <button type="button" className='button form-button-pill' value={'Yes'} onClick={(e) => { setSeizure(e.target.value) }}>Yes</button>
             <button type="button" className='button form-button-pill' value={'No'} onClick={(e) => { setSeizure(e.target.value) }}>No</button>
-          </fieldset>
-          <button className='btn btn-primary font-size-16 m-3 p-2 text-uppercase'>Submit</button>
+          </fieldset> */}
+          {/* <button className='btn btn-primary font-size-16 m-3 p-2 text-uppercase' type='submit'>Submit</button> */}
         </form>
       </Form>
-    </div>
+    </SeizureComponent>
   )
 }
 
-Seizure = connect()(Seizure)
-export default Seizure
+SeizureAssessment = connect()(SeizureAssessment)
+export default SeizureAssessment;
--- src/pages/SeizureMonitoring/index.js
+++ src/pages/SeizureMonitoring/index.js
@@ -1,1 +1,18 @@
-export { default } from "./Seizure"
+import React from 'react'
+import TopBar from '../../components/form/TopBar'
+import PropTypes from "prop-types"
+
+const SeizureComponent = (props) => {
+    SeizureComponent.propTypes = {
+        children: PropTypes.any,
+        backroute:PropTypes.string,
+    }
+  return (
+    <div>
+          <TopBar title='Seizure Tracking' route={`${props.backroute}`}/>
+          {props.children}
+    </div>
+  )
+}
+
+export default SeizureComponent
