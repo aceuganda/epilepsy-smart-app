d4ed0f90735f412f67745e620a548b1c0b37c33f
FaithDaka <faithbusigu8@gmail.com>
2022-09-27 19:40:21 +0300

    Button with pop up plus file linting

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,35 +1,34 @@
-import React, { useEffect } from 'react'
-import PropTypes from "prop-types";
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-const Modal = ({children, show, closeModal}) => {
-    Modal.propTypes ={
-        children: PropTypes.any,
-        show: PropTypes.bool,
-        closeModal: PropTypes.func,
+const Modal = ({ children, show, closeModal }) => {
+  Modal.propTypes = {
+    children: PropTypes.any,
+    show: PropTypes.bool,
+    closeModal: PropTypes.func
+  };
+
+  const toggleModal = () => {
+    if (show) {
+      document.getElementById('modal-wrapper').style.display = 'block';
+      document.getElementById('modal').style.display = 'block';
+    } else {
+      document.getElementById('modal-wrapper').style.display = 'none';
+      document.getElementById('modal').style.display = 'none';
     }
+  };
 
-    const toggleModal = () =>{
-        if (show) {
-            document.getElementById('modal-wrapper').style.display = "block"
-            document.getElementById('modal').style.display = "block"
-        }
-        else {
-            document.getElementById('modal-wrapper').style.display = "none"
-            document.getElementById('modal').style.display = "none"
-        }
-    }
+  useEffect(() => {
+    toggleModal();
+  }, [show]);
 
-    useEffect(()=>{
-        toggleModal();
-    }, [show])
-
-    return (
-        <div className='modal-wrapper' id='modal-wrapper' onClick={closeModal}>
-            <div className='modal' id='modal'>
-                {children}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="modal-wrapper" id="modal-wrapper" onClick={closeModal}>
+      <div className="modal" id="modal">
+        {children}
+      </div>
+    </div>
+  );
+};
 
 export default Modal;
