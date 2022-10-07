c1bb3b7d855b72aa891594b74c5681452f5382ca
Mike Nsubuga <nsubugamike021@gmail.com>
2022-10-07 09:28:40 +0300

    Added templating for page three Resilience

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,5 +1,5 @@
-import React, { lazy, Suspense } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
 import Onboarding from './pages/Onboarding';
 import Notify from './pages/Onboarding/notify';
@@ -10,38 +10,38 @@ import PageThree from './pages/SeizureMonitoring/SeizureAssessment/PageThree';
 import PageTwo from './pages/SeizureMonitoring/SeizureAssessment/PageTwo';
 import ResiliencePageOne from './pages/ResilienceTracking/ResiliencePageOne';
 import ResiliencePageTwo from './pages/ResilienceTracking/ResiliencePageTwo';
-
+import ResiliencePageThree from './pages/ResilienceTracking/ResiliencePageThree';
 
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 
 const AppRoutes = () => {
-    return (
-        <Router>
-            <Suspense fallback=
-                {
-                    <div className="d-flex justify-content-center text-center load-spinner">
-                        <div className="loader" role="status">
-                            <span className="sr-only">Loading...</span>
-                        </div>
-                    </div>
-                }>
-                <Routes>
-                    <Route path="/" element={<LandingPage />} />
-                    <Route path="/home" element={<HomePage />} />
-                    <Route path="/onboarding" element={<Onboarding />} />
-                    <Route path="/onboarding/notify" element={<Notify />} />
-                    <Route path="/onboarding/track" element={<Track />} />
-                    <Route path="/seizure-form" element={<StartPage />} />
-                    <Route path="/seizure-form/assessment/1" element={<PageOne />} />
-                    <Route path="/seizure-form/assessment/2" element={<PageTwo />} />
-                    <Route path="/seizure-form/assessment/3" element={<PageThree />} />
-                    <Route path="/resilience-form/1" element={<ResiliencePageOne />} />
-                    <Route path="/resilience-form/2" element={<ResiliencePageTwo />} />
+  return (
+    <Router>
+      <Suspense
+        fallback={
+          <div className="d-flex justify-content-center text-center load-spinner">
+            <div className="loader" role="status">
+              <span className="sr-only">Loading...</span>
+            </div>
+          </div>
+        }>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/onboarding" element={<Onboarding />} />
+          <Route path="/onboarding/notify" element={<Notify />} />
+          <Route path="/onboarding/track" element={<Track />} />
+          <Route path="/seizure-form" element={<StartPage />} />
+          <Route path="/seizure-form/assessment/1" element={<PageOne />} />
+          <Route path="/seizure-form/assessment/2" element={<PageTwo />} />
+          <Route path="/seizure-form/assessment/3" element={<PageThree />} />
+          <Route path="/resilience-form/1" element={<ResiliencePageOne />} />
+          <Route path="/resilience-form/2" element={<ResiliencePageTwo />} />
+          <Route path="/resilience-form/3" element={<ResiliencePageThree />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
+};
 
-                </Routes>
-            </Suspense>
-        </Router>
-    )
-}
-
-export default AppRoutes
+export default AppRoutes;
