47e120c2780911c4b0d8908271ccf27ed4831079
Mike Nsubuga <nsubugamike021@gmail.com>
2022-10-08 11:32:00 +0300

    update api routes for resilience tracking

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 import { SEIZURE_URL } from '../config/urls';
+import { RESILIENCE_URL } from '../config/urls';
 
 export const postSeizureAssessment = async (data) =>
   await axios.post(SEIZURE_URL, data).then((response) => response.data);
+
+export const postResilience = async (data) =>
+  await axios.post(RESILIENCE_URL, data).then((response) => response.data);
