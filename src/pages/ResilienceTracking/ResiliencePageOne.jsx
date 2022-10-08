e1399b7f2cf73ffe1e69716070c7ba88e98c7a3b
Mike Nsubuga <nsubugamike021@gmail.com>
2022-10-08 18:09:33 +0300

    removed classnames

--- src/pages/ResilienceTracking/ResiliencePageOne.jsx
+++ src/pages/ResilienceTracking/ResiliencePageOne.jsx
@@ -48,67 +48,27 @@ const ResiliencePageOne = () => {
           {engaged_socially !== null ? (
             <Question question={'How did you engage today?'}>
               <fieldset className="mt-3 mb-4">
-                <div className="flex items-center mb-4">
+                <div className="flex mb-4">
                   <input
                     id="default-checkbox"
                     type="checkbox"
                     value="School"
-                    onClick={(e) => {
+                    onChange={(e) => {
                       setEngagedType(e.target.value);
                     }}
-                    className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                   />
-                  <label
-                    htmlFor="default-checkbox"
-                    className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                    School
-                  </label>
+                  <label htmlFor="default-checkbox">School</label>
                 </div>
-                <div className="flex items-center">
+                <div className="flex">
                   <input
-                    checked=""
                     id="checked-checkbox"
                     type="checkbox"
                     value="Family"
-                    onClick={(e) => {
+                    onChange={(e) => {
                       setEngagedType(e.target.value);
                     }}
-                    className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                   />
-                  <label
-                    htmlFor="checked-checkbox"
-                    className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                    Family
-                  </label>
-                </div>
-                <div className="flex items-center">
-                  <input
-                    checked=""
-                    id="checked-checkbox"
-                    type="checkbox"
-                    value="Friends visit at Home"
-                    className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                  />
-                  <label
-                    htmlFor="checked-checkbox"
-                    className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                    Friends visit at Home
-                  </label>
-                </div>
-
-                <div className="flex items-center">
-                  <input
-                    checked=""
-                    id="checked-checkbox"
-                    type="checkbox"
-                    value="Outing with my Friends"
-                    className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                  />
-                  <label
-                    htmlFor="checked-checkbox"
-                    className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                    Outing with my Friends
-                  </label>
+                  <label htmlFor="checked-checkbox">Family</label>
                 </div>
               </fieldset>
             </Question>
