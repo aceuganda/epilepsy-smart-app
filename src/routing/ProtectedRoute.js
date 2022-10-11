c620073779445e45a43ea9b8777d9b05f91d196a
Mike Nsubuga <nsubugamike021@gmail.com>
2022-10-11 21:08:54 +0300

    added styles to protectedRoute default page

--- src/routing/ProtectedRoute.js
+++ src/routing/ProtectedRoute.js
@@ -1,7 +1,11 @@
 //ProtectedRoute
 import { useSelector } from 'react-redux';
 import { NavLink, Outlet } from 'react-router-dom';
-
+const styles = {
+  button: {
+    backgroundColor: '#8c3e79',
+  }
+};
 const ProtectedRoute = () => {
   const { userInfo } = useSelector((state) => state.user);
   return (
@@ -9,14 +13,19 @@ const ProtectedRoute = () => {
       {userInfo ? (
         <Outlet />
       ) : (
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6 offset-md-3">
-              <div className="alert alert-danger">
-                You are not authorized to access this page. Please{' '}
-                <NavLink to="/login">login</NavLink> to continue.
-              </div>
-            </div>
+        <div className="onboarding">
+          <div className="notify-section" >
+            <div></div>
+          </div>
+          <div className="bottom-section">
+            <span className="content">
+              <p> You are not authorized to access this page</p>
+            </span>
+          </div>
+          <div>
+            <NavLink to="/login">
+              <button className='o-btn'>Click here to Login/Register</button>
+            </NavLink>
           </div>
         </div>
       )}
