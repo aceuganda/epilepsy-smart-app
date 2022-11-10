ad7d4109c032582c5370d9167a411614cd28830c
FaithDaka <faithbusigu8@gmail.com>
2022-11-10 21:49:27 +0000

    feat:resilience activities and routing

---
+++ src/pages/ResilienceActivities/AllActivitiesPage.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import ResilienceActivitiesPageComponent from '.';
+import Form from '../../components/form/Form';
+import ResilienceActivityPill from '../../components/form/ResilienceActivityPill';
+
+const AllResilienceActivitiesPage = () => {
+  const ResilienceActivities = [
+    {
+      name: 'One service goal',
+      icon: null,
+      link: null
+    },
+    {
+      name: 'Manage your triggers',
+      icon: null,
+      link: null
+    },
+    {
+      name: 'positive affirmations',
+      icon: null,
+      link: '/resilience-activities/positive-affirmations'
+    },
+    {
+      name: 'One social goal',
+      icon: null,
+      link: null
+    },
+    {
+      name: 'Secure diary',
+      icon: null,
+      link: null
+    },
+    {
+      name: 'Education Epilepsy',
+      icon: null,
+      link: null
+    },
+    {
+      name: 'Inspirational Cultural quotes',
+      icon: null,
+      link: null
+    },
+    {
+      name: 'General Well being',
+      icon: null,
+      link: null
+    }
+  ];
+  return (
+    <div>
+      <ResilienceActivitiesPageComponent backroute={'/home'}>
+        <Form>
+          {ResilienceActivities.map((activity, key) => (
+            <ResilienceActivityPill
+              key={key}
+              title={activity.name}
+              icon={activity.icon}
+              link={activity.link}
+            />
+          ))}
+        </Form>
+      </ResilienceActivitiesPageComponent>
+    </div>
+  );
+};
+
+export default AllResilienceActivitiesPage;
