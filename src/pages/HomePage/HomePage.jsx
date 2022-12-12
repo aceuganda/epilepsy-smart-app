c9dc4ecf271d81b222460df061dc4abbdc752681
Mike Nsubuga <nsubugamike021@gmail.com>
2022-12-12 14:47:29 +0000

    Added notifications and changed targetSDK to 33

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -11,10 +11,47 @@ import Avatar from './Avatar';
 import { Link } from 'react-router-dom';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { capitalise } from '../../utils';
+import { LocalNotifications } from '@capacitor/local-notifications';
+import quotes from '../../resources/insipiration_quotes.json';
 
 const HomePage = () => {
   const { userInfo } = useSelector((state) => state.user);
 
+  //request permission to send local notification
+  const sendLocalNotification = async () => {
+    const hasPermission = await LocalNotifications.requestPermissions();
+    if (hasPermission) {
+      LocalNotifications.schedule({
+        notifications: [
+          {
+            //select the notification title and body from the json file randomly
+            title: 'Quote of the day',
+            body: quotes[Math.floor(Math.random() * quotes.length)].title,
+            id: 1,
+            //schedule: { at: new Date(Date.now() + 5000) },
+            sound: null,
+            attachments: null,
+            actionTypeId: '',
+            extra: null
+          }
+        ]
+      });
+    }
+    console.log('hasPermission', hasPermission);
+  };
+
+  //call the function every after 8 hours
+  setInterval(() => {
+    sendLocalNotification();
+  }, 28800000);
+  
+  //You can use the below code to test the local notification functionality
+  /*
+  //call the function every 5 seconds
+  setInterval(() => {
+    sendLocalNotification();
+  }, 5000);
+*/
   return (
     <div className="home-page">
       <header className="row justify-content-between">
