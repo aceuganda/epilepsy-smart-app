802d2c3a81992d8d461e8e36cfa9efdd728a945c
FaithDaka <faithbusigu8@gmail.com>
2023-01-19 14:47:49 +0300

    resilience activities url config

--- src/apis/index.js
+++ src/apis/index.js
@@ -10,9 +10,13 @@ const config = {
     Authorization: `Bearer ${localStorage.getItem('userToken')}`
   }
 };
+axios.defaults.headers.common.Authorization = `JWT ${localStorage.getItem('userToken')}`;
 
 export const postSeizureAssessment = async (data) =>
   await axios.post(SEIZURE_URL, data, config).then((response) => response.data);
 
 export const postResilience = async (data) =>
   await axios.post(RESILIENCE_URL, data, config).then((response) => response.data);
+
+export const getResilienceRecord = async (id) =>
+  await axios.get(`${RESILIENCE_URL}/${id}`).then((response) => response.data);
