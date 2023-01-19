e872087e29bbfcfc4159db93e7f1968aef850b0d
FaithDaka <faithbusigu8@gmail.com>
2023-01-19 15:26:32 +0300

    lint checkbox file

--- src/components/form/CheckBox.jsx
+++ src/components/form/CheckBox.jsx
@@ -1,32 +1,28 @@
-import React from "react";
-import { useState } from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
 
-const CheckBox = ({ label,id,checked,onChange,value,...props }) => {
+const CheckBox = ({ label, id, checked, onChange, value, ...props }) => {
   CheckBox.propTypes = {
     id: PropTypes.string,
     label: PropTypes.string.isRequired,
     onChange: PropTypes.func,
-    checked : PropTypes.string,
-    value:PropTypes.string
+    checked: PropTypes.string,
+    value: PropTypes.string
   };
-  const defaultChecked =checked ? checked : false;
+  const defaultChecked = checked ? checked : false;
   // const [isChecked, setIsChecked] = useState(false);
 
   return (
-    <div >
-      <label  style={{display:"flex"   }} className="checkbox-wrapper">
-        <input 
-         id={id}
-         value={value}
-         type="checkbox" 
-         checked = {checked}
-        //  checked={isChecked}
-         onChange={onChange}
-        //  onChange={() => setIsChecked((prev) => !prev)}
-         {...props}
-        //  className={isChecked ? "checked" : ""}
-         />
+    <div>
+      <label style={{ display: 'flex' }} className="checkbox-wrapper">
+        <input
+          id={id}
+          value={value}
+          type="checkbox"
+          checked={checked}
+          onChange={onChange}
+          {...props}
+        />
         <span>{label}</span>
       </label>
     </div>
