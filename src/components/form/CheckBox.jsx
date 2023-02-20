00ef323d2d4c1cdf1ae0a9983fb7fc494a441626
FaithDaka <faithbusigu8@gmail.com>
2023-02-20 12:12:07 +0300

    feat: debug checkbox component

--- src/components/form/CheckBox.jsx
+++ src/components/form/CheckBox.jsx
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const CheckBox = ({ label, id, checked, onChange, value, ...props }) => {
+const CheckBox = ({ label, id, onClick, checked, ...props }) => {
   CheckBox.propTypes = {
-    id: PropTypes.string,
+    id: PropTypes.any,
     label: PropTypes.string.isRequired,
+    onClick: PropTypes.func,
     onChange: PropTypes.func,
-    checked: PropTypes.string,
-    value: PropTypes.string
+    checked: PropTypes.bool
   };
+
   const defaultChecked = checked ? checked : false;
-  // const [isChecked, setIsChecked] = useState(false);
+  const [isChecked, setIsChecked] = useState(defaultChecked);
 
   return (
     <div>
       <label style={{ display: 'flex' }} className="checkbox-wrapper">
         <input
-          id={id}
-          value={value}
           type="checkbox"
-          checked={checked}
-          onChange={onChange}
+          className={isChecked ? 'checked' : ''}
+          id={id}
+          checked={isChecked}
+          onClick={onClick}
+          onChange={() => setIsChecked((prev) => !prev)}
           {...props}
         />
         <span>{label}</span>
