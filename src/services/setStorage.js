64bafdb94a5d8f1d7ec4349072789f4c38052b27
FaithDaka <faithbusigu8@gmail.com>
2023-02-20 13:05:12 +0300

    remove redundant files

--- src/services/setStorage.js
+++
@@ -1,5 +0,0 @@
-import { useSeizureAssessmentData } from "../data/reduxStore/Seizure/selector";
-
-export const setLocalStorage = () =>{
-    localStorage.setItem('S-Responses', JSON.stringify(seizureData));
-}
