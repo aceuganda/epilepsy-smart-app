fec7d5af28db524c90b98899aad8b4c982288bfb
FaithDaka <faithbusigu8@gmail.com>
2023-02-21 12:59:18 +0300

    user account pages

---
+++ src/pages/UserSettingsPage/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserSettingsPageComponent from '.';
+
+const About = () => {
+  return (
+    <div>
+      <UserSettingsPageComponent backroute={'/account'}></UserSettingsPageComponent>
+    </div>
+  );
+};
+
+export default About;
