6d1623d8460255a57bcce3f3b055b94373591c69
FaithDaka <faithbusigu8@gmail.com>
2023-02-28 14:46:53 +0300

    registration review

--- src/pages/Authentication/Register.jsx
+++ src/pages/Authentication/Register.jsx
@@ -16,7 +16,7 @@ const Register = () => {
 
   const { loading, userInfo, error, success } = useSelector((state) => state.user);
   const [imageObject, setImageObject] = useState(null);
-  const [hasCaregiver, sethasCaregiver] = useState('no');
+  const [hasCaregiver, sethasCaregiver] = useState(null);
 
   const dispatch = useDispatch();
 
@@ -79,78 +79,67 @@ const Register = () => {
       linkTitle={'Login Now'}>
       <form onSubmit={handleSubmit(submitForm)}>
         <div className="form-group">
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            name="username"
-            {...register('username', { required: true })}
-            placeholder="Enter username"
-          />
-          {errors.username && <span className="error">Username is required</span>}
-        </div>
-        <div className="form-select-group">
-          <div>
-            <label htmlFor="age">Age</label>
-            <input
-              type="number"
-              min="10"
-              max="100"
-              {...register('age', { required: true })}
-              placeholder="10"
-            />
-            {errors.age && <span className="error">required field</span>}
-          </div>
-          <div>
-            <label htmlFor="gender">Gender</label>
-            <select {...register('gender', { required: true })}>
-              <option value="female">female</option>
-              <option value="male">male</option>
-              <option value="other">other</option>
-            </select>
-            {errors.gender && <span className="error">required field</span>}
-          </div>
-          <div>
-            <label htmlFor="age_of_onset">Age of onset</label>
-            <input
-              type="number"
-              min="0"
-              max="100"
-              {...register('age_of_onset', { required: true })}
-              placeholder="00"
-            />
-            {errors.ageOfOnset && <span className="error">required field</span>}
-          </div>
-        </div>
-        <div className="form-group">
           <label htmlFor="email">Email</label>
           <input
             type="email"
             name="email"
             {...register('email', { required: true })}
-            placeholder="Enter email"
+            placeholder="Enter your email address"
           />
-          {errors.email && <span className="error">Email is required</span>}
+          {errors.email && <span className="error">Your email is required</span>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            {...register('username', { required: true })}
+            placeholder="Choose a username"
+          />
+          {errors.username && <span className="error">Username is required</span>}
+        </div>
+        <div className="form-select-group">
+          <div>
+            <label htmlFor="dob">Date of Birth</label>
+            <input type="date" {...register('dob', { required: true })} />
+            {errors.age && <span className="error">This is required field</span>}
+          </div>
+          <div>
+            <label htmlFor="gender">Gender</label>
+            <select {...register('gender', { required: true })}>
+              <option value="female">Female</option>
+              <option value="male">Male</option>
+              <option value="other">Prefer not to say</option>
+            </select>
+            {errors.gender && <span className="error">This is a required field</span>}
+          </div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="age_of_onset">Age of seizure onset</label>
+          <input
+            type="number"
+            min="0"
+            max="100"
+            {...register('age_of_onset', { required: true })}
+            placeholder="00"
+          />
+          {errors.ageOfOnset && <span className="error">This is a required field</span>}
         </div>
         <div className="form-group">
           <label htmlFor="seizure_type">Seizure Type</label>
-          <input
-            type="text"
-            name="seizure_type"
+          <select
             {...register('seizure_type')}
-            placeholder="Enter type"
-          />
-          {errors.seizureType && <span className="error">Seizure type is required</span>}
+            style={{ width: '230px', paddingLeft: '2px', marginLeft: '10px' }}>
+            <option value="Tonic">Tonic</option>
+            <option value="Atonic">Atonic</option>
+            <option value="MyoClonic">MyoClonic</option>
+            <option value="Clonic">Clonic</option>
+          </select>
+          {errors.seizureType && <span className="error">Check this field</span>}
         </div>
         <div className="form-checkbox-group">
           <label htmlFor="caregiverOption">Do you have a caregiver?</label>
           <div>
-            <span
-              className={hasCaregiver === 'no' ? 'Selected' : ''}
-              onClick={() => {
-                sethasCaregiver('no');
-              }}>
-              No
-            </span>
             <span
               //empty "" to prevent console error
               className={hasCaregiver === 'yes' ? 'Selected' : ''}
@@ -159,28 +148,41 @@ const Register = () => {
               }}>
               Yes
             </span>
+            <span
+              className={hasCaregiver === 'no' ? 'Selected' : ''}
+              onClick={() => {
+                sethasCaregiver('no');
+              }}>
+              No
+            </span>
           </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="caregiver_name">Name of caregiver</label>
-          <input
-            type="text"
-            name="caregiver_name"
-            {...register('caregiver_name', { required: hasCaregiver === 'yes' })}
-            placeholder="Enter caregiver's name "
-          />
-          {errors.caregiverName && <span className="error">name is required</span>}
-        </div>
-        <div className="form-group">
-          <label htmlFor="caregiver_contact">Contact of caregiver</label>
-          <input
-            type="text"
-            name="caregiver_contact"
-            {...register('caregiver_contact', { required: hasCaregiver === 'yes' })}
-            placeholder="Enter caregiver's contact "
-          />
-          {errors.caregiverContact && <span className="error">contact is required</span>}
-        </div>
+        {hasCaregiver === 'yes' ? (
+          <div>
+            <div className="form-group">
+              <label htmlFor="caregiver_name">Name of Caregiver</label>
+              <input
+                type="text"
+                name="caregiver_name"
+                {...register('caregiver_name', { required: hasCaregiver === 'yes' })}
+                placeholder="Enter caregiver's name "
+              />
+              {errors.caregiverName && <span className="error">This field is required</span>}
+            </div>
+            <div className="form-group">
+              <label htmlFor="caregiver_contact">Contact of Caregiver</label>
+              <input
+                type="text"
+                name="caregiver_contact"
+                {...register('caregiver_contact', { required: hasCaregiver === 'yes' })}
+                placeholder="Enter caregiver's contact "
+              />
+              {errors.caregiverContact && <span className="error">This field is required</span>}
+            </div>
+          </div>
+        ) : (
+          <span />
+        )}
         <div className="form-group">
           <label htmlFor="institution">Institution</label>
           <input
@@ -192,7 +194,7 @@ const Register = () => {
           {errors.institution && <span className="error">Institution is required</span>}
         </div>
         <div className="form-group">
-          <label htmlFor="password">Password</label>
+          <label htmlFor="password">Create Password</label>
           <input
             type="password"
             name="password"
@@ -212,9 +214,9 @@ const Register = () => {
             {...register('confirmPassword', { required: true })}
             placeholder="Confirm password"
           />
-          {errors.confirmPassword && <span className="error">Confirm password is required</span>}
+          {errors.confirmPassword && <span className="error">Password does not match</span>}
         </div>
-        <div className="form-group">
+        {/* <div className="form-group">
           <section>
             <label htmlFor="profilePicture">Profile picture (optional) </label>
             <img
@@ -234,7 +236,7 @@ const Register = () => {
             />
             Upload image
           </label>
-        </div>
+        </div> */}
         <button disabled={loading} className="o-btn">
           {loading ? <Spinner /> : 'Register'}
         </button>
