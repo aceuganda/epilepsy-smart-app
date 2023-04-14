47898c43acd1941ecd1c0c49d572826e21164e14
FaithDaka <faithbusigu8@gmail.com>
2023-04-14 11:08:13 +0300

    CSS styling

--- src/pages/EmailVerification/VerificationPage.jsx
+++ src/pages/EmailVerification/VerificationPage.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import NotifyImg from '../../assets/img/Onboarding/verification.png';
+import VerifyImg from '../../assets/img/Onboarding/verification.png';
 import { Link } from 'react-router-dom';
 
 const VerificationPage = () => {
   return (
     <div className="onboarding">
-      <div className="notify-section">
-        <img
-          style={{ width: '378px', height: '350px', left: '2%' }}
-          src={NotifyImg}
-          alt="Quick Updates"
-        />
+      <div className="verify-section">
+        <img src={VerifyImg} alt="" />
       </div>
       <div className="bottom-section">
         <h4>Verify your email address</h4>
         <span className="content">
           <p>You&apos;ve entered example@gmail.com as the email address for your account.</p>
         </span>
-        {/* <img src={Dotted} /> */}
       </div>
-      <Link to="/onboarding/track">
+      <Link to="/home">
         <button className="o-btn">Verify your email</button>
       </Link>
     </div>
