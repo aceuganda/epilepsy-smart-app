7857b45836c1fc6cee1cf897052e714cbb4a0a95
khalifan-kfan <khalifanmuwonge@gmail.com>
2024-07-23 20:25:45 +0300

    chore: add a page not found page

---
+++ src/pages/PageNotFound/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import UserSettingsPageComponent from '../UserSettingsPage';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+
+const PageNotFound = () => {
+  const { t } = useTranslation();
+  return (
+    <UserSettingsPageComponent backroute={'/'} title={t('Page Not Found')}>
+      <div>Page Not Found</div>
+      <Link to={'/'} download>
+        <button style={{ backgroundColor: 'blueviolet', marginTop: '23px', borderRadius: '10px', color: 'white' }}>
+          {t('Home')}
+        </button>
+      </Link>
+    </UserSettingsPageComponent>
+  );
+};
+
+export default PageNotFound;
