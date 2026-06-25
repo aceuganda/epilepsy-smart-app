4f30f9914096ad74eea714e84be0221f3f323257
khalifan-kfan <khalifanmuwonge@gmail.com>
2026-06-25 13:10:33 +0300

    ch: update application bundles

--- src/pages/UserSettingsPage/UpdatePage.jsx
+++ src/pages/UserSettingsPage/UpdatePage.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import UserSettingsPageComponent from '.';
 import { ReactComponent as AboutImg } from '../../assets/svg/Resilience/resilience.svg';
 import { useTranslation } from 'react-i18next';
-import apkPath from './apks/smart_app_10.0.apk?url';
 
 const Updates = () => {
   const { t } = useTranslation();
@@ -10,19 +9,7 @@ const Updates = () => {
     <div>
       <UserSettingsPageComponent backroute={'/'} title={t('App Version')}>
         <AboutImg style={{ marginTop: '50px' }} />
-        <div>Current Version is (10.0)</div>
-        <a href={apkPath} download>
-          <button
-            style={{
-              backgroundColor: 'blueviolet',
-              marginTop: '23px',
-              borderRadius: '10px',
-              color: 'white',
-            }}
-          >
-            {t('Download version 10.0')}
-          </button>
-        </a>
+        <div>Current Version is (13)</div>
       </UserSettingsPageComponent>
     </div>
   );
